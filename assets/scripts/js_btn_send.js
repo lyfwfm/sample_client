20000c4028cd5f0849a56f75a88b67020df0ba87
lyfwfm <lyfwfm@163.com>
2019-03-08 11:27:31 +0800

    简易websocket client 加载就连接服务器

---
+++ assets/scripts/js_btn_send.js
@@ -0,0 +1,38 @@
+// Learn cc.Class:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] https://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        txt_content:{
+            default:null,
+            type:cc.EditBox
+        }
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+        this.node.on('click',this.onClick,this);
+    },
+
+    
+
+    start () {
+
+    },
+
+    onClick:function(event){
+        
+    },
+
+    // update (dt) {},
+});
